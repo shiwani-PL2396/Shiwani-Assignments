7b280eca9abd73d06efabee9e7668f031f884493
shiwani-PL2396 <suman301shk@gmail.com>
2025-05-20 14:02:56 +0530

    Add files via upload

---
+++ Assignment_2 Strings/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Assignment_2_Strings
+{
+    internal class Program
+    {
+        /* Write a program in C# to print individual characters of 
+       the string in reverse order */
+        static void Main(string[] args)
+        {
+            string str = "Hello Shiwani";
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            Console.WriteLine("Reversed string: " + new string(charArray));
+            Console.ReadLine();
+        }
+        static void PrintIndividualCharacters(string str)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        {
+         }
+    }
+}
