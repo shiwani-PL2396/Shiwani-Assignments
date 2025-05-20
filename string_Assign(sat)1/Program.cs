410d463e0127c492d62a0260b0cab47d338c88cf
shiwani-PL2396 <suman301shk@gmail.com>
2025-05-20 14:58:02 +0530

    Add files via upload

---
+++ string_Assign(sat)1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class RemoveElementsProgram
+{ /* From a given list of numbers, remove elements if:
+      It is an even number
+      It is a duplicate element */
+
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 2, 5, 6, 3, 7, 8 };
+
+        var filtered = numbers
+            .GroupBy(n => n)
+            .Where(g => g.Count() == 1 && g.Key % 2 != 0) // not duplicate, and odd
+            .Select(g => g.Key)
+            .ToList();
+
+        Console.WriteLine("Filtered List (odd & unique):");
+        Console.WriteLine(string.Join(", ", filtered));
+    }
+}
