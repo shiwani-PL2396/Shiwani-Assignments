1241089dba80792add232dfd79fabc157deb9785
shiwani-PL2396 <suman301shk@gmail.com>
2025-05-21 17:19:00 +0530

    Add files via upload

---
+++ Assign_3/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 0 1 2 3 4 5
+1 1 2 3 4 5
+2 2 4 6 8 10
+3 3 6 9 12 15
+...
+*/
+
+using System;
+
+class MultiplicationMatrix
+{
+    static void Main()
+    {
+        for (int row = 0; row <= 5; row++)
+        {
+            for (int col = 0; col <= 5; col++)
+            {
+                Console.Write($"{row * col}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Assign_4/Program.cs
@@ -0,0 +1,22 @@
+﻿/* 1
+   2 1
+   3 2 1
+   4 3 2 1
+*/
+
+using System;
+
+class NumberPattern
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 4; i++)
+        {
+            for (int j = i; j >= 1; j--)
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
